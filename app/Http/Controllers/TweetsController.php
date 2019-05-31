a313d25d711e554a7821dc619fe73aed64263d23
jolaolu <adebayoanjola@gmail.com>
2019-05-31 01:33:09 +0100

    feat: works well

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -1,16 +1,26 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use App\Tweet;
 
 class TweetsController extends Controller
 {
-    public $reply= 'you mentioned me, beautiful day it is today. Do have a great day!';
+    public $reply;
+    public $replyArray 
+        = [ 
+            'you mentioned me, beautiful day it is today. Do have a great day!',
+            'always at your service!', 
+            'Your picture looks really nice',
+            'What would you have me do boss!'
+        ];
+   
+  
     protected $client;
 
     public function index()
@@ -22,8 +32,9 @@ class TweetsController extends Controller
     }
 
 
-    function __construct(){
-      //creates a middleware for authenticating requests
+    function __construct()
+    {
+        //creates a middleware for authenticating requests
         $stack = HandlerStack::create();
 
         $middleware = new Oauth1(
@@ -48,9 +59,7 @@ class TweetsController extends Controller
 
     public function getTweets()
     {
-
-      $sinceId = 1;
-
+        $sinceId = Redis::get('TweetId') ?? 1;
           // Set the "auth" request option to "oauth" to sign using oauth
         $res = $this->client->get(
             'statuses/mentions_timeline.json',
@@ -65,39 +74,63 @@ class TweetsController extends Controller
 
         $data = json_decode($res->getBody());
 
-        //get the number of tweets returned, reduce it by 1 and set it as the new sinceId
+        //get the number of tweets returned then checks if there is a new tweet
 
         $tweetcount = sizeof($data);
-        $sinceId = $data[$tweetcount - 1]->id;
+      
+        if ($tweetcount == 0 ) {
+            echo('No new tweets, try again later');
+            return; 
+        } else {
+                Redis::set('TweetId', $sinceId);
+            
 
 
-        $tweets = [];
-        foreach ($data as $index => $mentions) {
+            $tweets = [];
+            foreach ($data as $index => $mentions) {
 
-          // pushes the data retrieved into an array
+                    // pushes the data retrieved into an array
 
-          array_push($tweets,[
-            'id' => !empty($mentions->id) ? $mentions->id : '',
-            'username' => !empty($mentions->user) ? $mentions->user->screen_name : ''
-          ]);
-        };
-        return $tweets;
+                    array_push($tweets,
+                        [
+                        'id' => !empty($mentions->id) ? $mentions->id : '',
+                        'username' => !empty($mentions->user) ? $mentions->user->screen_name : '',
+                        ]
+                    );
+
+
+            };
+                return $tweets;
+        }
     }
 
 
     public function postTweets($data)
     {
-      foreach ($data as $tweet) {
-        // code...
-        $res = $this->client->post('statuses/update.json',[
-          'query' => [
-            'status' => '@'.$tweet['username'].' Hi there, '.$this->reply,
-  				  'in_reply_to_status_id' => $tweet['id'],
-          ]
-        ]);
-      };
-
-      $data = json_decode($res->getBody());
+        $this->reply = array_rand($this->replyArray);
+     
+        foreach ($data as $tweet) {
+            // code...
+            $res = $this->client->post(
+                'statuses/update.json',
+                [
+                  'query' => [
+                    'status' => '@'.$tweet['username'].' Hi there, ' . $this->replyArray[$this->reply],
+                    'in_reply_to_status_id' => $tweet['id'],
+                  ]
+                ]
+            );
+            $data = json_decode($res->getBody());
+            $responseCode = $res->getStatusCode();
+            if ($responseCode=200) {
+                echo('your tweet was sent successfully');
+            } else {
+                echo('There was a problem sending your tweet');
+            };
+        };
+        
+        
+        
     }
 
 
